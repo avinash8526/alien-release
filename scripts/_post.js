7bd08fc9a771beb881f6902a7f19e65cbbf2b278
Avinash Agrawal <avinash@1MG-LPTP-067.local>
2018-08-28 15:15:37 +0530

    feat(package.json): Commitizen is implemented

---
+++ scripts/_post.js
@@ -0,0 +1,8 @@
+var source = require('shell-source');
+console.log(__dirname);
+// source(__dirname + 'init.sh', function (err) {
+//   console.log(__dirname);
+//   if (err) return console.error(err);
+
+
+// });
